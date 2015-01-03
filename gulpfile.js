c57dd25398e68aef34e7e3fa0f91d1ffcda0165b
selfdeceited <selfdeceited@gmail.com>
2015-01-03 20:24:41 +0300

    Initial Commit. Gulp requires gulp-less-sourcemap npm package.

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,31 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
-    less = require('gulp-less'),
+    less = require('gulp-less-sourcemap'),
     path = require('path');
     
-// run
-gulp.task('default', function() {
+gulp.task('default', ['less', 'uglify']);
+
+gulp.task('less', function() {
   
   // easier to read variable
   var themeName = 'gridly';
   
   // less
-  gulp.src('./assets/less/' + themeName + '.less')
+  return gulp.src('./assets/less/' + themeName + '.less')
     .pipe(less({ paths: [path.join(__dirname, 'less', 'includes')]}))
     .pipe(rename('style.css'))
     .pipe(gulp.dest('./assets/css/'));
+});
 
+gulp.task('uglify', function() {
+  
+  // easier to read variable
+  var themeName = 'gridly';
+  
   // uglify
-  gulp.src('./assets/js/' + themeName + '.js')
+  return gulp.src('./assets/js/' + themeName + '.js')
     .pipe(uglify())
     .pipe(rename(themeName + '.min.js'))          
     .pipe(gulp.dest('./assets/js/'));
-  
 });
